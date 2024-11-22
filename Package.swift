61838de2b8ae61b1768a4a370c7e7b7390b0f62a
Bruno da Gama Porciuncula <bgamap@gmail.com>
2024-11-22 13:08:37 -0300

    first commit

--- Package.swift
+++ Package.swift
@@ -1,12 +1,12 @@
 // swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
-import PackageDescription
 import CompilerPluginSupport
+import PackageDescription
 
 let package = Package(
     name: "TypeInferedFactory",
-    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .macCatalyst(.v13)],
+    platforms: [.macOS(.v14), .iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -19,7 +19,11 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.0-latest"),
+        .package(
+            url: "https://github.com/swiftlang/swift-syntax.git",
+            from: "600.0.0-latest"),
+        .package(
+            url: "https://github.com/Swinject/Swinject.git", from: "2.9.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -29,22 +33,28 @@ let package = Package(
             name: "TypeInferedFactoryMacros",
             dependencies: [
                 .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
-                .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
             ]
         ),
 
         // Library that exposes a macro as part of its API, which is used in client programs.
-        .target(name: "TypeInferedFactory", dependencies: ["TypeInferedFactoryMacros"]),
+        .target(
+            name: "TypeInferedFactory",
+            dependencies: ["TypeInferedFactoryMacros"]),
 
         // A client of the library, which is able to use the macro in its own code.
-        .executableTarget(name: "TypeInferedFactoryClient", dependencies: ["TypeInferedFactory"]),
+        .executableTarget(
+            name: "TypeInferedFactoryClient",
+            dependencies: ["TypeInferedFactory", "Swinject"]),
 
         // A test target used to develop the macro implementation.
         .testTarget(
             name: "TypeInferedFactoryTests",
             dependencies: [
                 "TypeInferedFactoryMacros",
-                .product(name: "SwiftSyntaxMacrosTestSupport", package: "swift-syntax"),
+                .product(
+                    name: "SwiftSyntaxMacrosTestSupport",
+                    package: "swift-syntax"),
             ]
         ),
     ]
--- Sources/TypeInferedFactoryClient/main.swift
+++ Sources/TypeInferedFactoryClient/main.swift
@@ -1,8 +1,74 @@
+import Foundation
+import Swinject
 import TypeInferedFactory
 
-let a = 17
-let b = 25
+public protocol TypeInferedBuildable {
+    associatedtype RequiredInitializationParameter
 
-let (result, code) = #stringify(a + b)
+    static func construct(
+        _ parameter: RequiredInitializationParameter
+    ) -> Self
+}
 
-print("The value \(result) was produced by the code \"\(code)\"")
+extension TypeInferedBuildable {
+    public static func construct(
+        _ parameter: RequiredInitializationParameter
+    ) -> Self {
+        assertionFailure("This method should be overridden by the conforming type.")
+        abort()
+    }
+}
+
+public protocol TypeInferedFactoryProtocol {
+    func make<Output, each T>(
+        _ value: repeat each T
+    ) -> Output where Output: TypeInferedBuildable, Output.RequiredInitializationParameter == (repeat each T)
+}
+
+final class Factory: TypeInferedFactoryProtocol {
+    func make<Output, each T>(
+        _ value: repeat each T
+    ) -> Output where Output: TypeInferedBuildable, Output.RequiredInitializationParameter == (repeat each T) {
+        let tuple = (repeat each value)
+        return Output.construct(tuple)
+    }
+}
+
+protocol Person {
+    var name: String { get }
+    var age: Int { get }
+}
+
+struct Author: Person {
+    let name: String
+    let age: Int
+}
+
+struct Actor: Person {
+    let name: String
+    let age: Int
+}
+
+extension Actor: TypeInferedBuildable {
+    typealias RequiredInitializationParameter = (String, Int)
+
+    static func construct(_ parameter: RequiredInitializationParameter) -> Actor {
+        Actor(name: parameter.0, age: parameter.1)
+    }
+}
+
+extension Author: TypeInferedBuildable {
+    typealias RequiredInitializationParameter = (String, Int)
+
+    static func construct(_ parameter: RequiredInitializationParameter) -> Author {
+        Author(name: parameter.0, age: parameter.1)
+    }
+}
+
+let factory = Factory()
+
+let author: Author = factory.make("Marting Fowler", 72)
+let actor: Actor = factory.make("Jack Nicholson", 80)
+
+print(author)
+print(actor)
