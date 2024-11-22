cdc52bf8e6ff38e40d986394bd0be0f5033544d6
Bruno da Gama Porciuncula <bgamap@gmail.com>
2024-11-22 16:52:58 -0300

    test!: complex init class macro testing

--- Tests/TypeInferedFactoryTests/TypeInferedFactoryTests.swift
+++ Tests/TypeInferedFactoryTests/TypeInferedFactoryTests.swift
@@ -13,7 +13,7 @@ let testMacros: [String: Macro.Type] = [
 #endif
 
 final class TypeInferedFactoryTests: XCTestCase {
-    func testSimpleStructMacro() throws {
+    func testSimpleStructMacroExpansion() throws {
         #if canImport(TypeInferedFactoryMacros)
         assertMacroExpansion(
             """
@@ -34,6 +34,67 @@ final class TypeInferedFactoryTests: XCTestCase {
 
                     static func construct(_ parameter: RequiredInitializationParameter) -> Developer {
                         Developer(name: parameter.0, age: parameter.1)
+                    }
+                }
+                """,
+            macros: testMacros
+        )
+        #else
+        throw XCTSkip("macros are only supported when running tests for the host platform")
+        #endif
+    }
+    
+    func testMultipleInitClassMacroExpansion() throws {
+        #if canImport(TypeInferedFactoryMacros)
+        assertMacroExpansion(
+            """
+            @FactoryBuildable
+            final class SimpleContainer {
+                let firstValue: Int
+                let secondValue: String
+                let description: String
+
+                init(firstValue: Int, secondValue: String, description: String, shouldRedact: Bool) {
+                    self.firstValue = shouldRedact ? -1 : firstValue
+                    self.secondValue = shouldRedact ? "" : secondValue
+                    self.description = shouldRedact ? "" : description
+                }
+
+                convenience init(firstValue: Int, secondValue: String) {
+                    self.init(firstValue: firstValue, secondValue: secondValue, description: "Default description")
+                }
+
+                convenience init(firstValue: Int) {
+                    self.init(firstValue: firstValue, secondValue: "Default String", description: "Default description")
+                }
+            }
+            """,
+            expandedSource: """
+                final class SimpleContainer {
+                    let firstValue: Int
+                    let secondValue: String
+                    let description: String
+
+                    init(firstValue: Int, secondValue: String, description: String, shouldRedact: Bool) {
+                        self.firstValue = shouldRedact ? -1 : firstValue
+                        self.secondValue = shouldRedact ? "" : secondValue
+                        self.description = shouldRedact ? "" : description
+                    }
+
+                    convenience init(firstValue: Int, secondValue: String) {
+                        self.init(firstValue: firstValue, secondValue: secondValue, description: "Default description")
+                    }
+
+                    convenience init(firstValue: Int) {
+                        self.init(firstValue: firstValue, secondValue: "Default String", description: "Default description")
+                    }
+                }
+
+                extension SimpleContainer: TypeInferedFactoryBuildable {
+                    typealias RequiredInitializationParameter = (Int, String, String, Bool)
+
+                    static func construct(_ parameter: RequiredInitializationParameter) -> SimpleContainer {
+                        SimpleContainer(firstValue: parameter.0, secondValue: parameter.1, description: parameter.2, shouldRedact: parameter.3)
                     }
                 }
                 """,
